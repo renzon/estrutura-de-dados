b4e1bacde3ec55dfe88cca6e5f6f57edea5a0956
renzon <renzon@gmail.com>
2016-02-24 10:27:37 -0300

    Testes Automáticos

---
+++ aula3/testes.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+def soma(param, param1):
+    return param + param1
+
+
+class SomaTestes(unittest.TestCase):
+    def test_inteiro(self):
+        resultado = soma(1, 2)
+        self.assertEqual(4, resultado)
+
+    def test_float(self):
+        resultado = soma(1.0, 2.0)
+        self.assertEqual(3.0, resultado)
+
+if __name__=='__main__':
+    unittest.main()
