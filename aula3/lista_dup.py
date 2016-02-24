591bdfff43e7e531c6cd189a8f0d8fda98d3f64b
renzon <renzon@gmail.com>
2016-02-23 22:04:55 -0300

    Exercício de lista duplamente ligada. Testes de Noh

---
+++ aula3/lista_dup.py
@@ -0,0 +1,43 @@
+import unittest
+
+
+class Noh():
+    pass
+
+
+class Lista():
+    pass
+
+
+class NohTestes(unittest.TestCase):
+    def test_init_com_valores_padrao(self):
+        noh = Noh(4)
+        self.assertEqual(4, noh.valor)
+        self.assertIsNone(noh.esquerdo)
+        self.assertIsNone(noh.direito)
+
+    def test_init_com_no_esquerdo(self):
+        esquerdo = Noh(1)
+        noh = Noh(2, esquerdo)
+        self.assertEqual(esquerdo, noh.esquerdo)
+        self.assertIsNone(noh.direito)
+        noh3 = Noh(3, esquerdo=esquerdo)
+        self.assertEqual(esquerdo, noh3.esquerdo)
+        self.assertIsNone(noh3.direito)
+
+    def test_init_com_no_direito(self):
+        direito = Noh(1)
+        noh = Noh(2, direito=direito)
+        self.assertEqual(direito, noh.direito)
+        self.assertIsNone(noh.esquerdo)
+
+    def test_init_com_no_esquerdo_e_direito(self):
+        esquerdo = Noh(1)
+        direito = Noh(2)
+        noh = Noh(3, esquerdo, direito)
+        self.assertEqual(esquerdo, noh.esquerdo)
+        self.assertEqual(direito, noh.direito)
+
+
+if __name__ == '__main__':
+    unittest.main()
