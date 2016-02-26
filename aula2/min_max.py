1555bcc0bbf5a0b45d0ee79a243a07b827500d0f
renzon <renzon@gmail.com>
2016-02-26 10:52:59 -0300

    Colocando testes na função min_max e acrescentando informações

--- aula2/min_max.py
+++ aula2/min_max.py
@@ -1,3 +1,10 @@
+# A função min_max deverá rodar em O(n) e o código não pode usar nenhuma
+# lib do Python (sort, min, max e etc)
+# Não pode usar qualquer laço (while, for), a função deve ser recursiva
+# Ou delegar a solução para uma função puramente recursiva
+import unittest
+
+
 def min_max(seq):
     '''
     :param seq: uma sequencia
@@ -8,3 +15,14 @@ def min_max(seq):
     O segundo é o valor máximo (max) da sequencia
     '''
     pass
+
+
+class MinMaxTestes(unittest.TestCase):
+    def test_lista_vazia(self):
+        self.assertTupleEqual((None, None), min_max([]))
+
+    def test_lista_len_1(self):
+        self.assertTupleEqual((1, 1), min_max([1]))
+
+    def test_lista_consecutivos(self):
+        self.assertTupleEqual((0, 500), min_max([range(501)]))
