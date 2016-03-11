d37263961a5e86cbdb033f94ad514e5cbf96367d
renzon <renzon@gmail.com>
2016-03-11 17:21:41 -0300

    Testes de Análise Léxica

---
+++ aula5/exercicio.py
@@ -0,0 +1,113 @@
+class ErroLexico(Exception):
+    pass
+
+
+def analise_lexica(expressao):
+    """
+    Executa análise lexica transformando a expressao em fila de objetos:
+    Transforma inteiros em ints
+    Flutuantes em floats
+    e verificar se demais caracteres são validos: +-*/(){}[]
+    :param expressao: string com expressao a ser analisada
+    :return: fila com tokens
+    """
+    pass
+
+
+import unittest
+
+
+class AnaliseLexicaTestes(unittest.TestCase):
+    def test_caracter_estranho(self):
+        self.assertRaises(ErroLexico, analise_lexica, 'a')
+        self.assertRaises(ErroLexico, analise_lexica, 'ab')
+
+    def test_inteiro_com_um_algarismo(self):
+        fila = analise_lexica('1')
+        self.assertEqual(1, fila.desenfileirar())
+        self.assertTrue(fila.vazia())
+
+    def test_inteiro_com_vários_algarismos(self):
+        fila = analise_lexica('1234567890')
+        self.assertEqual(1234567890, fila.desenfileirar())
+        self.assertTrue(fila.vazia())
+
+    def test_float(self):
+        fila = analise_lexica('1234567890.34')
+        self.assertEqual(1234567890.34, fila.desenfileirar())
+
+    def test_parenteses(self):
+        fila = analise_lexica('(1)')
+        self.assertEqual('(', fila.desenfileirar())
+        self.assertEqual(1, fila.desenfileirar())
+        self.assertEqual(')', fila.desenfileirar())
+        self.assertTrue(fila.vazia())
+
+    def test_chaves(self):
+        fila = analise_lexica('{(1)}')
+        self.assertEqual('{', fila.desenfileirar())
+        self.assertEqual('(', fila.desenfileirar())
+        self.assertEqual(1, fila.desenfileirar())
+        self.assertEqual(')', fila.desenfileirar())
+        self.assertEqual('}', fila.desenfileirar())
+        self.assertTrue(fila.vazia())
+
+    def test_colchetes(self):
+        fila = analise_lexica('[{(1.0)}]')
+        self.assertEqual('[', fila.desenfileirar())
+        self.assertEqual('{', fila.desenfileirar())
+        self.assertEqual('(', fila.desenfileirar())
+        self.assertEqual(1.0, fila.desenfileirar())
+        self.assertEqual(')', fila.desenfileirar())
+        self.assertEqual('}', fila.desenfileirar())
+        self.assertEqual(']', fila.desenfileirar())
+        self.assertTrue(fila.vazia())
+
+    def test_adicao(self):
+        fila = analise_lexica('1+2.0')
+        self.assertEqual(1, fila.desenfileirar())
+        self.assertEqual('+', fila.desenfileirar())
+        self.assertEqual(2.0, fila.desenfileirar())
+        self.assertTrue(fila.vazia())
+
+    def test_subtracao(self):
+        fila = analise_lexica('1+2.0')
+        self.assertEqual(1, fila.desenfileirar())
+        self.assertEqual('-', fila.desenfileirar())
+        self.assertEqual(2.0, fila.desenfileirar())
+        self.assertTrue(fila.vazia())
+
+    def test_multiplicacao(self):
+        fila = analise_lexica('1*2.0')
+        self.assertEqual(1, fila.desenfileirar())
+        self.assertEqual('*', fila.desenfileirar())
+        self.assertEqual(2.0, fila.desenfileirar())
+        self.assertTrue(fila.vazia())
+
+    def test_divisao(self):
+        fila = analise_lexica('1/2.0')
+        self.assertEqual(1, fila.desenfileirar())
+        self.assertEqual('/', fila.desenfileirar())
+        self.assertEqual(2.0, fila.desenfileirar())
+        self.assertTrue(fila.vazia())
+
+    def test_expresao_com_todos_simbolos(self):
+        fila = analise_lexica('1/{2.0+3*[7-(5-3)]}')
+        self.assertEqual(1, fila.desenfileirar())
+        self.assertEqual('/', fila.desenfileirar())
+        self.assertEqual('{', fila.desenfileirar())
+        self.assertEqual(2.0, fila.desenfileirar())
+        self.assertEqual('+', fila.desenfileirar())
+        self.assertEqual(3, fila.desenfileirar())
+        self.assertEqual('*', fila.desenfileirar())
+        self.assertEqual('[', fila.desenfileirar())
+        self.assertEqual(7, fila.desenfileirar())
+        self.assertEqual('-', fila.desenfileirar())
+        self.assertEqual('(', fila.desenfileirar())
+        self.assertEqual(5, fila.desenfileirar())
+        self.assertEqual('-', fila.desenfileirar())
+        self.assertEqual(3, fila.desenfileirar())
+        self.assertEqual(')', fila.desenfileirar())
+        self.assertEqual(']', fila.desenfileirar())
+        self.assertEqual('}', fila.desenfileirar())
+        self.assertTrue(fila.vazia())
