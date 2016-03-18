12add961b05e3e69748f8c82d49f3f3da731bd1f
renzon <renzon@gmail.com>
2016-03-18 15:02:06 -0300

    Código base de tesde de ordenaçãoi

---
+++ aula6/ordenacao_testes.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+def ordenar(seq):
+    pass
+
+
+class OrdenacaoTestes(unittest.TestCase):
+    def teste_lista_vazia(self):
+        self.assertListEqual([], ordenar([]))
+
+    def teste_lista_unitaria(self):
+        self.assertListEqual([1], ordenar([1]))
+
+    def teste_lista_binaria(self):
+        self.assertListEqual([1, 2], ordenar([2, 1]))
+
+    def teste_lista_binaria(self):
+        self.assertListEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], ordenar([9, 7, 1, 8, 5, 3, 6, 4, 2, 0]))
+
+
+if __name__ == '__main__':
+    unittest.main()
