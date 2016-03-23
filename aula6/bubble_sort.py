d6e7444005bebc682434727619ba436ac6af75ab
renzon <renzon@gmail.com>
2016-03-23 09:56:39 -0300

    Código base de teste para BubbleSort

---
+++ aula6/bubble_sort.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+def bubble_sort(seq):
+    pass
+
+
+class OrdenacaoTestes(unittest.TestCase):
+    def teste_lista_vazia(self):
+        self.assertListEqual([], bubble_sort([]))
+
+    def teste_lista_unitaria(self):
+        self.assertListEqual([1], bubble_sort([1]))
+
+    def teste_lista_binaria(self):
+        self.assertListEqual([1, 2], bubble_sort([2, 1]))
+
+    def teste_lista_binaria(self):
+        self.assertListEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], bubble_sort([9, 7, 1, 8, 5, 3, 6, 4, 2, 0]))
+
+
+if __name__ == '__main__':
+    unittest.main()
