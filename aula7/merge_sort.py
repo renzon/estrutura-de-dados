f90dce1864e9dd374a6f24f9103e42621a63e3de
renzon <renzon@gmail.com>
2016-03-25 01:34:18 -0300

    Testes de _merge

--- aula7/merge_sort.py
+++ aula7/merge_sort.py
@@ -1,8 +1,29 @@
 import unittest
 
 
+def _merge(seq_esquerda, seq_direita):
+    pass
+
+
 def merge_sort(seq):
     pass
+
+
+class MergeTestes(unittest.TestCase):
+    def test_listas_vazias(self):
+        self.assertListEqual([], _merge([], []))
+
+    def test_lista_uma_lista_unitaria(self):
+        self.assertListEqual([1], _merge([1], []))
+
+    def test_listas_unitarias(self):
+        self.assertListEqual([1], _merge([1], []))
+
+    def test_listas_unitarias(self):
+        self.assertListEqual([1, 2], _merge([1], [2]))
+
+    def test_listas_intercaladas(self):
+        self.assertListEqual([1, 2, 3, 4, 5, 6], _merge([1, 3, 5], [2, 4, 6]))
 
 
 class OrdenacaoTestes(unittest.TestCase):
