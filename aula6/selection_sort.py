8681c4e60f5ad18bd116aa7208e9b8f53f9b736c
renzon <renzon@gmail.com>
2016-03-29 09:53:09 -0300

    Alterando teste duplicado

--- aula6/selection_sort.py
+++ aula6/selection_sort.py
@@ -19,7 +19,7 @@ class OrdenacaoTestes(unittest.TestCase):
     def teste_lista_binaria(self):
         self.assertListEqual([1, 2], selection_sort([2, 1]))
 
-    def teste_lista_binaria(self):
+    def teste_lista_desordenada(self):
         self.assertListEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], selection_sort([9, 7, 1, 8, 5, 3, 6, 4, 2, 0]))
 
 
