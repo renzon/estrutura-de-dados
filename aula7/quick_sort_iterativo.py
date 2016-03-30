6592df7cd7fde31be5396fa6772be81543f222e0
renzon <renzon@gmail.com>
2016-03-30 10:36:55 -0300

    Quick inline

---
+++ aula7/quick_sort_iterativo.py
@@ -0,0 +1,42 @@
+import unittest
+
+seq = [1, 1, 2]
+
+
+def _quick_recursivo(seq, inicio, final):
+    if inicio >= final:
+        return seq
+    indice_pivot = final
+    pivot = seq[-1]
+    i_menor = inicio
+    i_maior = final - 1
+
+    # posicionar pivot
+
+    # Resolver para sublista da esquerda
+
+    # Resolver para sublista da direita
+
+    return seq
+
+
+def quick_sort(seq):
+    return _quick_recursivo(seq, 0, len(seq) - 1)
+
+
+class OrdenacaoTestes(unittest.TestCase):
+    def teste_lista_vazia(self):
+        self.assertListEqual([], quick_sort([]))
+
+    def teste_lista_unitaria(self):
+        self.assertListEqual([1], quick_sort([1]))
+
+    def teste_lista_binaria(self):
+        self.assertListEqual([1, 2], quick_sort([2, 1]))
+
+    def teste_lista_desordenada(self):
+        self.assertListEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], quick_sort([9, 7, 1, 8, 5, 3, 6, 4, 2, 0]))
+
+
+if __name__ == '__main__':
+    unittest.main()
