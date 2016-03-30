1fa36c970f7261f3ec95ac93985bda1f561cee77
renzon <renzon@gmail.com>
2016-03-29 22:37:06 -0300

    Código base de busca binária

---
+++ aula8/busca_binaria.py
@@ -0,0 +1,36 @@
+def busca_binaria(seq, procurado):
+    """
+    Deve retornar o índice onde o elemento deveriar ser inserido em lista ordenada
+    :param procurado: elemento a ser procurado
+    :param seq: sequencia a ser pesquisada
+
+    :return: int
+    """
+    pass
+
+
+import unittest
+
+
+class BuscaBinariaTestes(unittest.TestCase):
+    def teste_lista_vazia(self):
+        self.assertEqual(0, busca_binaria([], 1))
+        self.assertEqual(0, busca_binaria([], 2))
+        self.assertEqual(0, busca_binaria([], 3))
+
+    def teste_lista_unitaria(self):
+        self.assertEqual(0, busca_binaria([1], 0))
+        self.assertEqual(0, busca_binaria([1], 1))
+        self.assertEqual(1, busca_binaria([1], 2))
+        self.assertEqual(1, busca_binaria([1], 3))
+        self.assertEqual(1, busca_binaria([1], 4))
+
+    def teste_lista_nao_unitaria(self):
+        lista = list(range(10))
+        self.assertEqual(0, busca_binaria(lista, -2))
+        self.assertEqual(0, busca_binaria(lista, -1))
+        for i in lista:
+            self.assertEqual(i, busca_binaria(lista, i))
+        self.assertEqual(10, busca_binaria(lista, 10))
+        self.assertEqual(10, busca_binaria(lista, 11))
+        self.assertEqual(10, busca_binaria(lista, 12))
