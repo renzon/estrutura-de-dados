de3df5fd78df199a3c0c52bbe6c95c636526ca1c
renzon <renzon@gmail.com>
2016-04-05 10:04:02 -0300

    Adicionando testes a ordenacao QuickSort iterativa

--- aula7/quick_sort_iterativo.py
+++ aula7/quick_sort_iterativo.py
@@ -35,6 +35,12 @@ class OrdenacaoTestes(unittest.TestCase):
     def teste_lista_desordenada(self):
         self.assertListEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], quick_sort([9, 7, 1, 8, 5, 3, 6, 4, 2, 0]))
 
+    def teste_lista_com_elementos_repetidos(self):
+        self.assertListEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9], quick_sort([9, 7, 1, 8, 5, 3, 6, 4, 2, 0, 9, 9]))
+
+    def teste_lista_so_com_elementos_repetidos(self):
+        self.assertListEqual([9, 9, 9], quick_sort([9, 9, 9]))
+
 
 if __name__ == '__main__':
     unittest.main()
