d8b31ffd3b1f6877c7718d749f35f8fed2e2f372
renzon <renzon@gmail.com>
2016-04-12 15:32:36 -0300

    Testes da arvore de Huffman

--- aula10/huffman.py
+++ aula10/huffman.py
@@ -2,6 +2,14 @@ def calcular_frequencias(s):
     pass
 
 
+def gerar_arvore_de_huffman(s):
+    pass
+
+
+def codificar(cod_dict, s):
+    pass
+
+
 class Noh:
     def __hash__(self):
         return hash(self.peso)
@@ -20,6 +28,16 @@ class Folha():
         if other is None or not isinstance(other, Folha):
             return False
         return self.__dict__ == other.__dict__
+
+
+class Arvore(object):
+    def __hash__(self):
+        return hash(self.raiz)
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.raiz == other.raiz
 
 
 from unittest import TestCase
@@ -61,6 +79,65 @@ class NohTestes(TestCase):
         self.assertIsNone(noh.direito)
 
 
+def _gerar_arvore_aaaa_bb_c():
+    raiz = Noh(7)
+    raiz.esquerdo = Folha('a', 4)
+    noh = Noh(3)
+    raiz.direito = noh
+    noh.esquerdo = Folha('b', 2)
+    noh.direito = Folha('c', 1)
+    arvore_esperada = Arvore()
+    arvore_esperada.raiz = raiz
+    return arvore_esperada
+
+
 class ArvoreTestes(TestCase):
     def teste_init(self):
-        pass
+        arvore = Arvore()
+        self.assertIsNone(arvore.raiz)
+
+    def teste_init(self):
+        arvore = Arvore('a', 3)
+        self.assertEqual(Folha('a', 3), arvore.raiz)
+
+    def teste_fundir_arvores_iniciais(self):
+        raiz = Noh(3)
+        raiz.esquerdo = Folha('b', 2)
+        raiz.direito = Folha('c', 1)
+        arvore_esperada = Arvore()
+        arvore_esperada.raiz = raiz
+
+        arvore = Arvore('b', 2)
+        arvore2 = Arvore('c', 1)
+        arvore_fundida = arvore.fundir(arvore2)
+        self.assertEqual(arvore_esperada, arvore_fundida)
+
+    def teste_fundir_arvores_nao_iniciais(self):
+        arvore_esperada = _gerar_arvore_aaaa_bb_c()
+
+        arvore = Arvore('b', 2)
+        arvore2 = Arvore('c', 1)
+        arvore3 = Arvore('a', 4)
+        arvore_fundida = arvore.fundir(arvore2)
+        arvore_fundida = arvore3.fundir(arvore_fundida)
+
+        self.assertEqual(arvore_esperada, arvore_fundida)
+
+    def teste_gerar_dicionario_de_codificacao(self):
+        arvore = _gerar_arvore_aaaa_bb_c()
+        self.assertDictEqual({'a': '0', 'b': '10', 'c': '11'}, arvore.cod_dict())
+
+    def teste_decodificar(self):
+        arvore = _gerar_arvore_aaaa_bb_c()
+        self.assertEqual('aaaabbc', arvore.decodificar('0000101011'))
+
+
+class TestesDeIntegracao(TestCase):
+    def teste_gerar_arvore_de_huffman(self):
+        arvore = _gerar_arvore_aaaa_bb_c()
+        self.assertEqual(arvore, gerar_arvore_de_huffman('aaaabbcc'))
+
+    def teste_codificar(self):
+        arvore = gerar_arvore_de_huffman('aaaabbcc')
+        self.assertEqual('0000101011', codificar(arvore.cod_dict(), 'aaaabbcc'))
+        self.assertEqual('aaaabbc', arvore.decodificar('0000101011'))
