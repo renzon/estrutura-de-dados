a5d0092cc37d5f231fa66e2406edee53c25a1f01
renzon <renzon@gmail.com>
2016-04-19 12:25:41 -0300

    Navegação em arvore binário com generator

---
+++ arvore.py
@@ -0,0 +1,36 @@
+class Arvore:
+    def __init__(self, valor, esquerda=None, direita=None):
+        self.direita = direita
+        self.esquerda = esquerda
+        self.valor = valor
+
+    def __iter__(self):
+        yield self.valor
+        if self.esquerda is not None:
+            for v in self.esquerda:
+                yield v
+        if self.direita is not None:
+            for v in self.direita:
+                yield v
+
+
+
+arvores = [Arvore(i) for i in range(10)]
+arvores[5].esquerda = arvores[2]
+arvores[5].direita = arvores[8]
+
+arvores[2].esquerda = arvores[1]
+arvores[2].direita = arvores[4]
+
+arvores[1].esquerda = arvores[0]
+
+arvores[4].esquerda = arvores[3]
+
+arvores[8].esquerda = arvores[7]
+arvores[8].direita = arvores[9]
+
+arvores[7].esquerda = arvores[6]
+
+# [5, 2, 1, 0, 4, 3, 8, 7, 6, 9]
+
+print(list(arvores[5]))
