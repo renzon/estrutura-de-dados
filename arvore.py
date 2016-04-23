21eab50ff9a6b4571fff1269e422fa00d4c28c33
renzon <renzon@gmail.com>
2016-04-23 15:30:36 -0300

    Testes de exercício alfanumérico

--- arvore.py
+++ arvore.py
@@ -1,36 +1,55 @@
-class Arvore:
-    def __init__(self, valor, esquerda=None, direita=None):
-        self.direita = direita
-        self.esquerda = esquerda
-        self.valor = valor
+# class Arvore:
+#     def __init__(self, valor, esquerda=None, direita=None):
+#         self.direita = direita
+#         self.esquerda = esquerda
+#         self.valor = valor
+#
+#     def __iter__(self):
+#         yield self.valor
+#         if self.esquerda is not None:
+#             for v in self.esquerda:
+#                 yield v
+#         if self.direita is not None:
+#             for v in self.direita:
+#                 yield v
+#
+#
+#
+# arvores = [Arvore(i) for i in range(10)]
+# arvores[5].esquerda = arvores[2]
+# arvores[5].direita = arvores[8]
+#
+# arvores[2].esquerda = arvores[1]
+# arvores[2].direita = arvores[4]
+#
+# arvores[1].esquerda = arvores[0]
+#
+# arvores[4].esquerda = arvores[3]
+#
+# arvores[8].esquerda = arvores[7]
+# arvores[8].direita = arvores[9]
+#
+# arvores[7].esquerda = arvores[6]
+#
+# # [5, 2, 1, 0, 4, 3, 8, 7, 6, 9]
+#
+# print(list(arvores[5]))
 
-    def __iter__(self):
-        yield self.valor
-        if self.esquerda is not None:
-            for v in self.esquerda:
-                yield v
-        if self.direita is not None:
-            for v in self.direita:
-                yield v
+class A:
+    def __init__(self, a):
+        self.a = a
+
+    def __hash__(self):
+        return hash(self.a)
+
+    def __eq__(self, other):
+        if other is None or not isinstance(other, A):
+            return False
+        return self.a == other.a
 
 
-
-arvores = [Arvore(i) for i in range(10)]
-arvores[5].esquerda = arvores[2]
-arvores[5].direita = arvores[8]
-
-arvores[2].esquerda = arvores[1]
-arvores[2].direita = arvores[4]
-
-arvores[1].esquerda = arvores[0]
-
-arvores[4].esquerda = arvores[3]
-
-arvores[8].esquerda = arvores[7]
-arvores[8].direita = arvores[9]
-
-arvores[7].esquerda = arvores[6]
-
-# [5, 2, 1, 0, 4, 3, 8, 7, 6, 9]
-
-print(list(arvores[5]))
+a = A('a')
+dct = {a: 'a'}
+print(dct[a])
+a.a='b'
+print(dct[a])
---
+++ aula12/alfa.py
@@ -0,0 +1,34 @@
+import unittest
+from itertools import product
+
+regra = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wzyz'}
+
+
+def gerar_alfa(s):
+    """
+    Fazer análise de tempo e espaço
+
+    Função recebe string com números de 2 a 9 e responde todas sequencias possíveis de letras
+    Reg
+    """
+    pass
+
+
+class Testes(unittest.TestCase):
+    def testes_string_vazia(self):
+        self.assertListEqual([], gerar_alfa(''))
+
+    def testes_string_2(self):
+        self.assertListEqual([('a',), ('b',), ('c',)], list(gerar_alfa('2')))
+
+    def testes_string_3(self):
+        self.assertListEqual([('d',), ('e',), ('f',)], list(gerar_alfa('3')))
+
+    def testes_string_com_2_numeros(self):
+        self.assertSetEqual(set((('a', 'd'), ('a', 'e'), ('a', 'f'), ('b', 'd'), ('b', 'e'), ('b', 'f'), ('c', 'd'),
+                                 ('c', 'e'), ('c', 'f'))), set(gerar_alfa('3')))
+
+    def testes_com_5_numeros(self):
+        resultado = set(gerar_alfa('73696'))
+        self.assertIn(tuple('renzo'), resultado)
+        self.assertSetEqual(list(product('pqrs', 'def', 'mno', 'wzyz', 'mno')), resultado)
