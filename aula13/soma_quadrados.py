fec6bcf1ce1d9f8209e6634e3fc46cd7a88e66c2
renzon <renzon@gmail.com>
2016-04-29 11:37:37 -0300

    teste soma quadrados pefeitos

---
+++ aula13/soma_quadrados.py
@@ -0,0 +1,34 @@
+def soma_quadrados(n):
+    pass
+
+
+import unittest
+
+
+class SomaQuadradosPerfeitosTestes(unittest.TestCase):
+    def teste_0(self):
+        self.assert_possui_mesmo_elementos([0], soma_quadrados(0))
+
+    def teste_1(self):
+        self.assert_possui_mesmo_elementos([1], soma_quadrados(1))
+
+    def teste_2(self):
+        self.assert_possui_mesmo_elementos([1, 1], soma_quadrados(0))
+
+    def teste_3(self):
+        self.assert_possui_mesmo_elementos([1, 1, 1], soma_quadrados(0))
+
+    def teste_4(self):
+        self.assert_possui_mesmo_elementos([4], soma_quadrados(4))
+
+    def teste_5(self):
+        self.assert_possui_mesmo_elementos([4, 1], soma_quadrados(5))
+
+    def teste_11(self):
+        self.assert_possui_mesmo_elementos([9, 1, 1], soma_quadrados(11))
+
+    def teste_12(self):
+        self.assert_possui_mesmo_elementos([4, 4, 4], soma_quadrados(12))
+
+    def assert_possui_mesmo_elementos(self, esperado, resultado):
+        self.assertSetEqual(set(esperado), set(resultado))
